41e850b8d9b0975ea35dbfd009368663413cd779
alexvb92 <a_vonbredow@live.com>
2017-08-27 18:21:45 -0400

    init

---
+++ main.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function() {
+    $(".about").on('click', function() {
+        $(".display1").html("<div><img class='circle javaimg' src='test.jpg' /><br />My name is Alex von Bredow, and I like to make websites</div>")
+    });
+    $(".portfolio").on('click', function() {
+        $(".display1").html("<div><img class='circle javaimg' src='test.jpg' /><br />This is where all my portfolio stuff is gonna be</div>")
+    });
+    $(".contact").on('click', function() {
+        $(".display1").html("<div><img class='circle javaimg' src='test.jpg' /><br />yo uc an co nt ac tm eh er e</div>")
+    });
+
+});
